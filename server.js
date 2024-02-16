d4c8abf58dcb0b6d0e6c242b806180d3cf21aa41
monsosam <monsonsamuel5@gmail.com>
2024-02-15 20:40:48 -0700

    h

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ app.use(require('./routes'));
 
 // Connect mongoose
 mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/social-network-api', {
-    // useFindAndModify: false,
+    
     useNewUrlParser: true,
     useUnifiedTopology: true
 }).then(() => {
