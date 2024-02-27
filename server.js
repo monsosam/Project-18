2ef8ebcb530d0dc0fe47b1eddb97404cffff39d3
monsosam <monsonsamuel5@gmail.com>
2024-02-26 17:38:00 -0700

    server.js edit

--- server.js
+++ server.js
@@ -1,10 +1,8 @@
-const mongoose = require('mongoose');
 const express = require('express');
+const mongoose = require('mongoose');
 require('dotenv').config();
 
 
-const express = require('express');
-const mongoose = require('mongoose');
 const app = express();
 const PORT = process.env.PORT || 3001;
 
